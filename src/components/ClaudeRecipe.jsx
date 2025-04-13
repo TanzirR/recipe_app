55091d59b4ac735514ab175d36f4c2dd7c2f2223
Tanzir Razzaque <44029202+TanzirR@users.noreply.github.com>
2025-04-14 05:07:34 +0600

    Import ReactMarkdown

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -1,7 +1,9 @@
+import ReactMarkdown from "react-markdown"
 export default function ClaudeRecipe(props){
+   
     return (
-        <section>
-            {props.recipe}
+        <section className="suggested-recipe-container">
+            <ReactMarkdown>{props.recipe}</ReactMarkdown>
         </section>
     );
 }
