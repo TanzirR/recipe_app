2aa029f9b2b4425f6a259f87b7744759ee33a253
Tanzir Razzaque <44029202+TanzirR@users.noreply.github.com>
2025-04-14 04:08:35 +0600

    Update ClaudeRecipe.jsx

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -1,5 +1,7 @@
 export default function ClaudeRecipe(props){
     return (
-        props.recipe && <section>Section goes here</section>
-    )
+        <section>
+            {props.recipe}
+        </section>
+    );
 }
